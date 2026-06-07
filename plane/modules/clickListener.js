28739cf474f4bf31de6118f8ae4bcc9114a68390
Fania Everitt <me@fania.eu>
2026-06-07 16:27:56 +0100

    progress with shapes

--- plane/modules/clickListener.js
+++ plane/modules/clickListener.js
@@ -6,8 +6,7 @@ console.log(`There are a total of ${blocks.length} colour blocks.`);
 // console.log(`And there are ${gradients.length} unique gradients.`);
 
 
-
-
+let coords_list = [];
 
 
 my_svg.addEventListener("click", (ev) => {
@@ -15,6 +14,10 @@ my_svg.addEventListener("click", (ev) => {
   // const elem = document.elementFromPoint(ev.offsetX, ev.offsetY);
   // console.log(elem);
   console.log(`(${ev.offsetX}, ${ev.offsetY})`);
+  coords_list.push(`${ev.offsetX} ${ev.offsetY}`);
+  // navigator.clipboard.writeText(`${ev.offsetX} ${ev.offsetY}`);
+  // console.log(coords_list);
+  // console.log(coords_list.join(' '));
 });
 
 
@@ -26,3 +29,15 @@ blocks.forEach(block => {
     // console.log(elem.id);
   });
 });
+
+
+
+document.addEventListener("keydown", event => {
+  if (event.key === "x") {
+    console.log(coords_list);
+    navigator.clipboard.writeText(coords_list.join(' '));
+    coords_list = [];
+  }
+});
+
+// 309 3532 465 3544 375 3659
